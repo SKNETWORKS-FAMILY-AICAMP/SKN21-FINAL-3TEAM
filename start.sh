daf5bf106df0e1c23bf941280cb26d8171f30f91
jse <ya7016@naver.com>
2026-02-19 22:04:19 +0900

    TEST : CICD test

---
+++ start.sh
@@ -0,0 +1,4 @@
+  #!/bin/bash                                                                                                                                                                                                                                                           cd /home/ubuntu/SKN21-FINAL-3TEAM                                                                                                                                                                                                                                   
+  export PYTHONPATH=/home/ubuntu/SKN21-FINAL-3TEAM
+  exec /home/ubuntu/SKN21-FINAL-3TEAM/.venv/bin/uvicorn backend.app.main:app \
+    --host 0.0.0.0 --port 8000
